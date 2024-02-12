01ca3916d1f93bb92a999a27e57c0d097019dfa2
jalpanakrani1997 <159773285+jalpanakrani1997@users.noreply.github.com>
2024-02-12 17:02:25 +0530

    Add files via upload

---
+++ patten2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+main(){
+	printf("*\n*\n*\n");
+	printf("*                   *   *                        *\n");
+	printf("*               *           *                 *\n");
+	printf("*           *                   *         *\n");
+	printf("*       *                           *  *\n");
+	printf("*    *\n");
+	printf("*");
+}
+//*
+//* 
+//* 
+//*                   *   *                        *
+//*               *           *                 *
+//*           *                   *         *
+//*       *                           *  *
+//*    *
+//*
