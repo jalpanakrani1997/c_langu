a46590263bfe70d9ee6263889330e65054811696
jalpanakrani1997 <159773285+jalpanakrani1997@users.noreply.github.com>
2024-03-21 20:05:33 +0530

    Add files via upload

---
+++ lecture11.1_q1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main()
+{
+    int i, no;
+
+    printf("\nEnter the array's size: ");
+    scanf("%d", &no);
+
+    int a[no];
+
+    printf("\nEnter array's elements: \n");
+    for (i = 0; i < no; i++)
+    {
+        printf("a[%d] = ", i);
+        scanf("%d", &a[i]);
+    }
+
+    int *ptr[no];
+
+    printf("\nSquare of each element:\n");
+    for (i = 0; i < no; i++)
+    {
+        ptr[i] = &a[i];
+    }
+
+    for (i = 0; i < no; i++)
+    {
+        printf("%d, ", *ptr[i] * *ptr[i]);
+    }
+    printf("\n\n");
+}
