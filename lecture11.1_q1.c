49adc2f828ea0b03bd76c94e481eb8f5b1269059
jalpanakrani1997 <159773285+jalpanakrani1997@users.noreply.github.com>
2024-03-22 13:01:44 +0530

    Update lecture11.1_q1.c

--- lecture11.1_q1.c
+++ lecture11.1_q1.c
@@ -17,14 +17,11 @@ int main()
 
     int *ptr[no];
 
-    printf("\nSquare of each element:\n");
-    for (i = 0; i < no; i++)
-    {
-        ptr[i] = &a[i];
-    }
+    printf("\nSquare of each element:\n");   
 
     for (i = 0; i < no; i++)
     {
+        ptr[i] = &a[i];
         printf("%d, ", *ptr[i] * *ptr[i]);
     }
     printf("\n\n");
